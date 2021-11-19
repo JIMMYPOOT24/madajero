6e49b98ea5045961377d524d3ce088dbb7bbd684
Alberto-M117 <81482305+Alberto-M117@users.noreply.github.com>
2021-11-19 10:53:29 -0500

    se cambio la plantilla de correo ya es responsive

--- resources/views/email/contactanos.blade.php
+++ resources/views/email/contactanos.blade.php
@@ -1,63 +1,62 @@
 <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Correo Madajero</title>
+    <style>
 
-<style>
-
-@media only screen and (max-width:600px) {
-      .es-content table,
-      .es-footer table, 
-      .adapt-img { width:100%!important; height:auto!important }
+      body {
+        font-family: "Raleway", sans-serif;
+        background-color: #d8dada;
+        font-size: 19px;
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 3%;
       }
 
-.margin{
-    margin: 30%;
-}
-</style>
+      #wrapper {
+        background-color: #f0f6fb;
+      }
 
-<head>
-    <title>
-        Madajero Correo
-    </title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-</head>
 
-<body bgcolor="black">
+    </style>
+  </head>
+  <body>
+    <div id="wrapper">
+      
+        <header>
 
-    <section style="margin-left: 20%; margin-right: 20%;">
-        <div align-item="center">
-            <div style="background-color: white; padding-left:60px; padding-right:60px; width: 600px; height: auto; align-items: center">
+        <div style="padding-top: 40px; padding-left:30px; padding-right:30px;">
+            <a href='#' target='_blank'><img src='https://i.postimg.cc/Kz8nm81t/Grupo-1.png' alt='Grupo-1' width="250"/></a>
+        </div>
 
-                <div style="padding-top: 40px;">
-                    <img src="{{url('img/logos/correologo.png')}}" alt="madajero" width="300">
-                </div>
-            
-                <div style="font-family:sans-serif;line-height:17px;color:#333333; text-align: justify; padding-top: 10px;">
+        
+      </header>
+      
+        <div style="font-family:sans-serif;line-height:17px;color:#333333; text-align: justify; padding-top: 10px; padding-left:30px; padding-right:30px;">
                     <h3>Nombre:   {{$contacto['name']}}       </h3>
                     <h3>Numero telefonico: {{$contacto['number']}}    </h3>
                     <h3>Correo electronico:   {{$contacto['email']}}       </h3>
                     <h3>Mensaje:  {{$contacto['message']}}      </h3>
 
-                </div>
-
-    
-                <div>
-                    <p style="padding-top: 30px; font-family:sans-serif;line-height:17px;color:#333333;font-size:11px; text-align: center;">
-                        MADAJERO GRUPO CONSTRUCTOR S.A. DE C.V., CANCUN, QUINTANA ROO
-                        <br>XXX-XXX-XXXX</p>
-                </div>
-
-                <div style="padding-bottom:10px; text-align: center;">
-                    <p style="font-family:sans-serif;line-height:17px;color:#333333;font-size:11px">
-                    © MADAJERO GRUPO CONSTRUCTOR 2021
-                    </p>
-                </div>
-
-    
-                
-
-            </div>
         </div>
-    </section>
 
-</body>
+
+        <footer>
+            <div>
+                <p style="padding-top: 30px; font-family:sans-serif;line-height:17px;color:#333333;font-size:11px; text-align: center;">
+                    MADAJERO GRUPO CONSTRUCTOR S.A. DE C.V., CANCUN, QUINTANA ROO
+                    <br>XXX-XXX-XXXX</p>
+            </div>
+
+            <div style="padding-bottom:10px; text-align: center;">
+                <p style="font-family:sans-serif;line-height:17px;color:#333333;font-size:11px">
+                © MADAJERO GRUPO CONSTRUCTOR 2021
+                </p>
+            </div>
+          
+        </footer>
+      </div>
+  </body>
 </html>
