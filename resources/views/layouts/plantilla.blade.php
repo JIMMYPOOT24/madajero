dd3323cdf499943dd2f0c71625027fec9bd2a73a
Alberto-M117 <81482305+Alberto-M117@users.noreply.github.com>
2021-11-18 22:56:09 -0500

    se cambio el tamaño de las letras del logo

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -31,7 +31,7 @@
     <nav id="navbar" class="navbar fixed-top navbar-expand-lg navbar-light p-md-3">
       <div class="container">
         <a class="navbar-brand" href="{{route('index')}}">
-          <img src="img/logos/logo-white.png" alt="logo-white" width="150">
+          <img src="img/logos/logo-white-3.png" alt="logo-white" width="220">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
