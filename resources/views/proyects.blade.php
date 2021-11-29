8d66c929145e6552d727d35f4e86d7389913f5b8
Jimmy Can Poot <jimmypoot24@gmail.com>
2021-11-29 09:52:18 -0500

    Coregidoo

--- resources/views/proyects.blade.php
+++ resources/views/proyects.blade.php
@@ -62,35 +62,41 @@
       <br>
       <span class="divider"></span>
       <h1 class="lines-effect fs-1">{{$proyectos->titulo}}</h1> </div>
-    <div class="container">
-      <div class="row p-2">
+    <div class="container p-0">
+      <div class="row p-1 gx-2">
         <div class="col-sm">
-          <img src="storage/{{$proyectos->url_img}}" class="gallery-item"  alt="gallery"> 
+          <img src="storage/{{$proyectos->url_img}}" class="gallery-item"  alt="gallery" style="display: none" onload="this.style.display=''" > 
         </div>
         <div class="col-sm">
-          <img src="storage/{{$proyectos->url_img2}}" class="gallery-item" alt="gallery"> 
+          <img src="storage/{{$proyectos->url_img2}}" style="display: none" onload="this.style.display=''"  class="gallery-item" alt="gallery"> 
+
+          
         </div>
         <div class="col-sm">
-          <img src="storage/{{$proyectos->url_img3}}" class="gallery-item" alt="gallery"> 
+          <img src="storage/{{$proyectos->url_img3}}" class="gallery-item" alt="gallery" style="display: none" onload="this.style.display=''" > 
         </div>
       </div>
 
-      <div class="row  p2">
+      <div class="row p1 gx-2">
         <div class="col-sm">
-          <img src="storage/{{$proyectos->url_img4}}" class="gallery-item"  alt="gallery"> 
+          <img src="storage/{{$proyectos->url_img4}}" class="gallery-item"  alt="gallery" style="display: none" onload="this.style.display=''" > 
         </div>
         <div class="col-sm">
-          <img src="storage/{{$proyectos->url_img5}}" class="gallery-item" alt="gallery"> 
+          <img src="storage/{{$proyectos->url_img5}}" class="gallery-item" alt="gallery" style="display: none" onload="this.style.display=''" > 
         </div>
         <div class="col-sm">
-          <img src="storage/{{$proyectos->url_img6}}" class="gallery-item" alt="gallery"> 
+          <img src="storage/{{$proyectos->url_img6}}" class="gallery-item" alt="gallery" style="display: none" onload="this.style.display=''" > 
         </div>
       </div>
      
     </div>
     @endforeach
        
-      
+      <script>
+        
+     
+
+      </script>
     
     
     </section> 
