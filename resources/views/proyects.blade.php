6ec5c2c4b6d9d289a846bae844bb15d976bd4e23
Jimmy Can Poot <jimmypoot24@gmail.com>
2021-11-29 09:10:29 -0500

    Corregido Proeyctos, falta clientes

--- resources/views/proyects.blade.php
+++ resources/views/proyects.blade.php
@@ -56,40 +56,41 @@
   
    <!-- Carousel proyectos -->
    
-   <section class="gallery min-vh-100 p-0">
-    <div class="container-lg"> 
-       @foreach ($datostbproyecto  as $proyectos)
-       <div class="container text-center">
-        <div class="row gy-4 justify-content-start">
-          <div class="titulo">
-            <br>
-            <span class="divider"></span>
-            <h1 class="lines-effect fs-1">{{$proyectos->titulo}}</h1> </div>
-            <br>
-          <div class="col-4">
-            <img src="storage/{{$proyectos->url_img}}" class="gallery-item" alt="gallery"> 
-          </div>
-          <div class="col-4">
-            <img src="storage/{{$proyectos->url_img2}}" class="gallery-item" alt="gallery"> 
-          </div>
-          <div class="col-4">
-            <img src="storage/{{$proyectos->url_img3}}" class="gallery-item" alt="gallery"> 
-          </div>
-          <div class="col-4">
-            <img src="storage/{{$proyectos->url_img4}}" class="gallery-item" alt="gallery"> 
-          </div>
-
-          <div class="col-4">
-            <img src="storage/{{$proyectos->url_img5}}" class="gallery-item" alt="gallery"> 
-          </div>
-
-          <div class="col-4">
-            <img src="storage/{{$proyectos->url_img6}}" class="gallery-item" alt="gallery"> 
-          </div>
+   <section class="gallery text-center p-0">
+    @foreach ($datostbproyecto  as $proyectos)
+    <div class="titulo">
+      <br>
+      <span class="divider"></span>
+      <h1 class="lines-effect fs-1">{{$proyectos->titulo}}</h1> </div>
+    <div class="container">
+      <div class="row p-2">
+        <div class="col-sm">
+          <img src="storage/{{$proyectos->url_img}}" class="gallery-item"  alt="gallery"> 
         </div>
-         @endforeach
+        <div class="col-sm">
+          <img src="storage/{{$proyectos->url_img2}}" class="gallery-item" alt="gallery"> 
+        </div>
+        <div class="col-sm">
+          <img src="storage/{{$proyectos->url_img3}}" class="gallery-item" alt="gallery"> 
+        </div>
+      </div>
+
+      <div class="row  p2">
+        <div class="col-sm">
+          <img src="storage/{{$proyectos->url_img4}}" class="gallery-item"  alt="gallery"> 
+        </div>
+        <div class="col-sm">
+          <img src="storage/{{$proyectos->url_img5}}" class="gallery-item" alt="gallery"> 
+        </div>
+        <div class="col-sm">
+          <img src="storage/{{$proyectos->url_img6}}" class="gallery-item" alt="gallery"> 
+        </div>
       </div>
      
+    </div>
+    @endforeach
+       
+      
     
     
     </section> 
