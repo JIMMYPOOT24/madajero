194682e28473a574533e2b12b8c955e41f73d91d
Jimmy Can Poot <jimmypoot24@gmail.com>
2021-12-01 08:55:01 -0500

    mejorandoo..

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -38,12 +38,11 @@
   <div class="container" data-aos="fade-up">
 
    
-  <div class="row">
+  <div class="row text-justify" data-aos="fade-up">
     @foreach ( $datotbservicio as $servicios)
     <div class="col-md-6">
-      <div class="icon-box" data-aos="fade-up">
+      <div class="container text-justify icon-box" data-aos="fade-up">
         <i class="bi bi-bar-chart"></i>
-        
         <img src="storage/{{$servicios->url_img}}" alt="icono-proyectos-ejecutivos ">
         <h4 class="container">{{$servicios->categoria}}</h4>
         <ul>
